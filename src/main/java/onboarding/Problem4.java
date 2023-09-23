c5eeccc3973e60832cf6ffac8ad4c210812de248
xogus3492 <wkdxogus3492@gmail.com>
2023-09-23 16:55:17 +0900

    [Problem4] feat: 문자열을 필터링하여 반환하는 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -6,6 +6,12 @@ import onboarding.error.exception.common.LengthOutOfBoundsException;
 public class Problem4 {
     private static final int MIN_LENGTH = 1;
     private static final int MAX_LENGTH = 1000;
+    private static final char LOWER_CASE_A = 'a';
+    private static final char LOWER_CASE_Z = 'z';
+    private static final char UPPER_CASE_A = 'A';
+    private static final char UPPER_CASE_Z = 'Z';
+    private static final int LOWER_CASE = 0;
+    private static final int UPPER_CASE = 1;
 
     public static String solution(String word) {
         if (isIncorrectStringLength(word)) {
@@ -17,15 +23,23 @@ public class Problem4 {
 
     public static String generateConvertedString(String word) {
         String resultStr = "";
-
         for (int i = 0; i < word.length(); i++) {
             resultStr += getFilteredChar(word.charAt(i));
         }
-
         return resultStr;
     }
 
     public static char getFilteredChar(char c) {
+        if (c >= LOWER_CASE_A && c <= LOWER_CASE_Z) {
+            return convertChar(c, LOWER_CASE);
+        }
+        if (c >= UPPER_CASE_A && c <= UPPER_CASE_Z) {
+            return convertChar(c, UPPER_CASE);
+        }
+        return c;
+    }
+
+    public static char convertChar(char c, int charCase) {
         return ' ';
     }
 
