25ac71090f6254e9710c6436b6b657dcca46fbeb
xogus3492 <wkdxogus3492@gmail.com>
2023-09-23 16:00:03 +0900

    [Problem4] feat: 데이터 유효성 검증 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import onboarding.error.ErrorMessage;
+import onboarding.error.exception.common.LengthOutOfBoundsException;
+
 public class Problem4 {
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 1000;
+
     public static String solution(String word) {
         String answer = "";
+
+        if (isIncorrectStringLength(word)) {
+            throw new LengthOutOfBoundsException(ErrorMessage.LENGTH_OUT_OF_BOUNDS_P4);
+        }
+
         return answer;
+    }
+
+    public static boolean isIncorrectStringLength(String word) {
+        return word.length() < MIN_LENGTH || word.length() > MAX_LENGTH;
     }
 }
