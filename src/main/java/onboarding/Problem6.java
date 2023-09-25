7474e6bfa838b23d40a58637791835d48a11b488
xogus3492 <wkdxogus3492@gmail.com>
2023-09-25 14:03:02 +0900

    [Problem6] refactor: 검증 기능 검증 방식 변경

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,20 +6,21 @@ import onboarding.error.exception.problem6.ListSizeOutOfRange;
 import java.util.List;
 
 public class Problem6 {
-    private static final int MIN_SIZE = 1;
-    private static final int MAX_SIZE = 10000;
+    private static final int MIN_LIST_SIZE = 1;
+    private static final int MAX_LIST_SIZE = 10000;
 
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
 
-        if (isIncorrectListSize(forms)) {
-            throw new ListSizeOutOfRange(ErrorMessage.INCORRECT_NUMBER_OF_MEMBER);
-        }
+        verifyListSize(forms);
 
         return answer;
     }
 
-    public static boolean isIncorrectListSize(List<List<String>> forms) {
-        return forms.size() < MIN_SIZE || forms.size() > MAX_SIZE;
+    public static void verifyListSize(List<List<String>> forms) {
+        if (forms.size() < MIN_LIST_SIZE || forms.size() > MAX_LIST_SIZE) {
+            throw new ListSizeOutOfRange(ErrorMessage.INCORRECT_NUMBER_OF_MEMBER);
+        }
     }
+
 }
